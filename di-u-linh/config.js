81ded92fbf3dc4c42ae072f0f6e9529a4c20ab5f
Mạnh Dê <97293757+manh9a111@users.noreply.github.com>
2024-06-11 14:56:11 +0700

    Add files via upload

---
+++ di-u-linh/config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "Diệu Linh cute <3",
+    introTitle: 'Diệu Linh',
+    introDesc: `ĐỒ MẮT HÍP`,
+    btnIntro: '^^HiHi^^',
+    title: 'yêu anh nha!!!',
+    desc: 'yêu anh thì anh không trêu mắt híp nữa',
+    btnYes: 'Tất nhiên là đồng ý rồiiii <33',
+    btnNo: 'Không nha :3',
+    question: 'Có nhớ anh không?',
+    btnReply: 'Gửi',
+    reply: 'có rất nhớ ',
+    mess: 'anh biết',
+    messDesc: 'Tối CN anh đưa đi chơi',
+    btnAccept: 'Okiii nhá <3',
+    messLink: 'https://manh9a111.github.io/xemphimclub-app/' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+}
